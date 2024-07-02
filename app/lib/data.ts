6eafd7ffbd81462a568f2518bddbc5c94418885d
mehrshad moha <mehrshadita@gmail.com>
2024-07-02 12:53:32 +0330

    deleted:    .env.example 	new file:   app/dashboard/(overview)/loading.tsx 	new file:   app/dashboard/(overview)/page.tsx 	modified:   app/dashboard/layout.tsx 	deleted:    app/dashboard/page.tsx 	modified:   app/lib/data.ts 	modified:   app/ui/dashboard/cards.tsx 	modified:   app/ui/dashboard/latest-invoices.tsx 	modified:   app/ui/dashboard/revenue-chart.tsx 	modified:   next.config.mjs 	modified:   package.json 	modified:   pnpm-lock.yaml

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,6 +41,9 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
